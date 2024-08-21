423cf3deb03ca01a3e886ad42902e284ab658345
yk-kamath <159676876+yk-kamath@users.noreply.github.com>
2024-08-21 21:20:50 +0530

    Automated commit for dim_security_oru.sql

---
+++ PM_PMA Azure Views/dim_security_oru.sql
@@ -0,0 +1,16 @@
+drop table DIM_SECURITY_ORU;
+
+/*==============================================================*/
+/* Table: DIM_SECURITY_ORU                                      */
+/*==============================================================*/
+create table DIM_SECURITY_ORU (
+USERID string,
+EMAIL string,
+CODE1ID string,
+CUBEID string,
+ORUID string,
+ORUID_TD string,
+ORUID_NCHAR string,
+INCLUDE_HEADCOUNT string,
+MODDT_UTC string
+);
