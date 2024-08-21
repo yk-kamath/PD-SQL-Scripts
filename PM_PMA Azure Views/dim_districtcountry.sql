0dac2ea7d630b15f522d4a8c60c7c15460b5b995
yk-kamath <159676876+yk-kamath@users.noreply.github.com>
2024-08-21 21:20:44 +0530

    Automated commit for dim_districtcountry.sql

---
+++ PM_PMA Azure Views/dim_districtcountry.sql
@@ -0,0 +1,13 @@
+drop table DIM_DISTRICTCOUNTR;
+
+/*==============================================================*/
+/* Table: DIM_DISTRICTCOUNTR                                    */
+/*==============================================================*/
+create table DIM_DISTRICTCOUNTR (
+ORUGROUP string,
+BMC string,
+BMCCODE string,
+COUNTRY_GROUP string,
+COUNTRY string,
+COUNTRYCODE string
+);
