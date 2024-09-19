04d71bc7960a66c562e91650b5010cfb7c34a77f
yk-kamath <159676876+yk-kamath@users.noreply.github.com>
2024-09-19 16:12:30 +0530

    Updated SQL script

--- PM Inventory Management/tbl_material_hierarchy.sql
+++ PM Inventory Management/tbl_material_hierarchy.sql
@@ -8,7 +8,7 @@ create table if not exists o2c.tbl_material_hierarchy (
    Client_Name          STRING,
    Product_Hierarchy    STRING,
    Loading_Date         DATE,
-   "Level"              STRING,
+   `Level`              STRING,
    Language_Key         STRING,
    Business_Group_Code  STRING,
    Business_Group_Name  STRING,
@@ -19,7 +19,7 @@ create table if not exists o2c.tbl_material_hierarchy (
    Article_Group_Code   STRING,
    Article_Group_Name   STRING,
    change_type          STRING,
-   "timeStamp"          TIMESTAMP
+   `timeStamp`          TIMESTAMP
 )
    USING delta
    LOCATION 'abfss://shared@az21q1datalakewe.dfs.core.windows.net/IM/O2C/silverl2/AzureDataLake/MasterData/T_MBP_Material_Hierarchy'
