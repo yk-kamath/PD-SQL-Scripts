eb40bb513bcda32c5b049cd8ec49c6d165f829f6
yk-kamath <159676876+yk-kamath@users.noreply.github.com>
2024-09-19 16:12:27 +0530

    Updated SQL script

--- PM Inventory Management/tbl_material_productionversions.sql
+++ PM Inventory Management/tbl_material_productionversions.sql
@@ -21,9 +21,9 @@ create table if not exists o2c.tbl_material_productionversions (
    Short_text_production_version STRING,
    Production_version_is_locked STRING,
    change_type          STRING,
-   "timeStamp"          TIMESTAMP,
+   `timeStamp`          TIMESTAMP,
    constraint FK_MATERIALPRODUCTIONVERSION_ITEM foreign key () references o2c.tbl_item () disable novalidate,
-   constraint FK_MATERIALPRODUCTIONVERSION_PLANT foreign key (Kernel_Name, Client_Name, Plant, change_type, "timeStamp") references o2c.tbl_plant (Kernel_Name, Client_Name, Plant, change_type, "timeStamp") disable novalidate
+   constraint FK_MATERIALPRODUCTIONVERSION_PLANT foreign key (Kernel_Name, Client_Name, Plant, change_type, `timeStamp`) references o2c.tbl_plant (Kernel_Name, Client_Name, Plant, change_type, `timeStamp`) disable novalidate
 )
    USING delta
    LOCATION 'abfss://wb-o2c-commercial@az21q1datalakewe.dfs.core.windows.net/silver/silverl2/O2C_WPP_CDM_Material_Productionversions'
