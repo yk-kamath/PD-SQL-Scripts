00945dec5e267b12c0ae962e38e8ff8cb8af3492
yk-kamath <159676876+yk-kamath@users.noreply.github.com>
2024-09-19 16:04:12 +0530

    Created SQL script

---
+++ PM Inventory Management/tbl_factory_calendar.sql
@@ -0,0 +1,31 @@
+drop table o2c.tbl_factory_calendar;
+
+/*==============================================================*/
+/* Table: tbl_factory_calendar                                  */
+/*==============================================================*/
+create table if not exists o2c.tbl_factory_calendar (
+   Kernel_Name          STRING,
+   Client_Name          STRING,
+   Plant                STRING,
+   CalendarID           STRING,
+   CalendarText         STRING,
+   Year_from_which_data_is_stored STRING,
+   Year_from_which_data_is_saved STRING,
+   Bit_for_workday_1    STRING,
+   Bit_for_workday_2    STRING,
+   Public_Holiday_Calendar STRING,
+   CalendarDate         DATE,
+   workDay              INT,
+   change_type          STRING,
+   "timeStamp"          TIMESTAMP,
+   constraint FK_FACTORYCALENDAR_PUBLICHOLIDAYCALENDAR foreign key (Kernel_Name, Public_Holiday_Calendar, change_type, "timeStamp") references o2c.tbl_public_holiday_calendar (Kernel_Name, Public_Holiday_Calendar, change_type, "timeStamp") disable novalidate,
+   constraint FK_FACTORYCALENDAR_PLANT foreign key (Client_Name, Plant) references o2c.tbl_plant (Client_Name, Plant) disable novalidate
+)
+   USING delta
+   LOCATION 'abfss://shared@az21q1datalakewe.dfs.core.windows.net/IM/O2C/silverl2/AzureDataLake/PlanToFinishedGoods/T_MBP_Factory_Calendar'
+   TBLPROPERTIES (
+     'delta.autoOptimize.autoCompact' = 'true',
+     'delta.autoOptimize.optimizeWrite' = 'true',
+     'delta.enableChangeDataFeed' = 'true',
+     'delta.minReaderVersion' = '1',
+     'delta.minWriterVersion' = '4');
